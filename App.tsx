e750439516d88081577a80999ae2772e96462b8c
Joaquin Bianchi <joacobian21@gmail.com>
2022-09-07 00:36:49 -0300

    in-app browser

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
-import {StyleSheet, View} from 'react-native';
+import {Linking, StyleSheet, View} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Amplify from 'aws-amplify';
 import config from 'src/aws-exports';
 import Navigation from '@navigation/index';
 import {AuthProvider} from '@context/AuthContext';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 
-Amplify.configure(config);
+async function urlOpener(url: string, redirectUrl: string) {
+  await InAppBrowser.isAvailable();
+  const response = await InAppBrowser.openAuth(url, redirectUrl, {
+    showTitle: false,
+    enableUrlBarHiding: true,
+    enableDefaultShare: false,
+    ephemeralWebSession: false,
+  });
+
+  if (response.type === 'success') {
+    Linking.openURL(response.url);
+  }
+}
+
+const newConfig = {...config, oauth: {...config.oauth, urlOpener}};
+
+Amplify.configure(newConfig);
 
 const App = () => {
   return (
