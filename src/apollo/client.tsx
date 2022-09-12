8f76c401b6d9ddece3a533be8b7ff23197c7410d
Joaquin Bianchi <joacobian21@gmail.com>
2022-09-12 00:14:16 -0300

    setting up apollo client

--- src/apollo/client.tsx
+++ src/apollo/client.tsx
@@ -1,11 +1,37 @@
-import {ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  createHttpLink,
+} from '@apollo/client';
+import {createAuthLink, AuthOptions, AUTH_TYPE} from 'aws-appsync-auth-link';
+import {createSubscriptionHandshakeLink} from 'aws-appsync-subscription-link';
+
+import config from 'src/aws-exports';
 
 interface IApolloClientProps {
   children: JSX.Element | JSX.Element[];
 }
 
+const url = config.aws_appsync_graphqlEndpoint;
+
+const region = config.aws_appsync_region;
+
+const auth: AuthOptions = {
+  type: config.aws_appsync_authenticationType as AUTH_TYPE.API_KEY,
+  apiKey: config.aws_appsync_apiKey,
+};
+
+const httpLink = createHttpLink({uri: url});
+
+const link = ApolloLink.from([
+  createAuthLink({url, region, auth}),
+  createSubscriptionHandshakeLink({url, region, auth}, httpLink),
+]);
+
 const client = new ApolloClient({
-  uri: 'https://flyby-gateway.herokuapp.com/',
+  link,
   cache: new InMemoryCache(),
 });
 
