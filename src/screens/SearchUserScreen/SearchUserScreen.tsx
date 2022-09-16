a81df01e5bf424f1a707246cdfaf90a6ed3bfa9d
Joaquin Bianchi <joacobian21@gmail.com>
2022-09-15 22:50:28 -0300

    adding list users query

--- src/screens/SearchUserScreen/SearchUserScreen.tsx
+++ src/screens/SearchUserScreen/SearchUserScreen.tsx
@@ -2,20 +2,44 @@ import {FlatList, View} from 'react-native';
 import React from 'react';
 import comments from '@assets/comments.json';
 import UserItem from '@components/UserItem';
+import {listSearchUsers} from './queries';
+import {useQuery} from '@apollo/client';
+import {ListUsersQuery, ListUsersQueryVariables} from 'src/API';
+import ApiErrorMessage from '@components/ApiErrorMessage';
+import Loading from '@components/Loading';
 
 const users = comments.map(comment => comment.user);
 
 const SearchUserScreen = () => {
+  const {data, loading, error, refetch} = useQuery<
+    ListUsersQuery,
+    ListUsersQueryVariables
+  >(listSearchUsers);
+
+  if (error) {
+    return (
+      <ApiErrorMessage
+        title="error searching users... "
+        message={error.message}
+        onRetry={refetch}
+      />
+    );
+  }
+
+  if (loading) {
+    return <Loading text="loading users..." />;
+  }
+
   return (
     <View>
       <FlatList
-        data={users}
+        data={data?.listUsers?.items}
         ItemSeparatorComponent={() => <View style={{padding: 5}} />}
         renderItem={({item}) => (
           <UserItem
-            username={item.username}
-            name={item.username}
-            image={item.image}
+            username={item?.username}
+            name={item?.name ?? ''}
+            image={item?.image}
           />
         )}
       />
