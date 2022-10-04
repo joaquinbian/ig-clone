e9078645480361c5724f02ca9987f3ce72a17a89
Joaquin Bianchi <joacobian21@gmail.com>
2022-10-03 21:00:31 -0300

    adding refresh control in feed screen

--- src/screens/FeedScreen/FeedScreen.tsx
+++ src/screens/FeedScreen/FeedScreen.tsx
@@ -6,6 +6,7 @@ import {
   View,
   ActivityIndicator,
   Text,
+  RefreshControl,
 } from 'react-native';
 import Post from '@components/Post';
 import {API, graphqlOperation} from 'aws-amplify';
@@ -25,7 +26,7 @@ const viewabilityConfig: ViewabilityConfig = {
 
 const FeedScreen = () => {
   const [currentItem, setCurrentItem] = useState<null | string>(null);
-  const {data, loading, error} = useQuery<
+  const {data, loading, error, refetch} = useQuery<
     ListPostsQuery,
     ListPostsQueryVariables
   >(listPosts, {});
@@ -33,24 +34,11 @@ const FeedScreen = () => {
   //esta es la funcion que especifica los items que estan en pantalla
   const onViewableItemsChanged = useRef(
     ({viewableItems}: IOnViewableItemsChanged) => {
-      //console.log('Visible items in feed screen are', viewableItems);
-      // console.log('Changed in this iteration', changed);
       if (viewableItems.length) {
         setCurrentItem(viewableItems[0].item.id);
       }
     },
   );
-  /* 
-  const fetchPosts = async () => {
-    const response = await API.graphql(graphqlOperation(listPosts));
-
-    setRemotePosts(response.data.listPosts.items);
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
- */
 
   if (error) {
     return (
@@ -74,6 +62,8 @@ const FeedScreen = () => {
       viewabilityConfig={viewabilityConfig}
       onViewableItemsChanged={onViewableItemsChanged.current}
       showsVerticalScrollIndicator={false}
+      refreshing={loading}
+      onRefresh={refetch}
     />
   );
 };
